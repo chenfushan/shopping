6a2c92a52be446ef764c20f8bc40512bf51cb096
chen <chenfushan1992@gmail.com>
2013-09-29 21:10:00 +0800

    test?

--- order.php
+++ order.php
@@ -34,20 +34,22 @@
 			<div class="process-text">Write your appraise</div>
 		</div>
 		<div id="order">
-			<div id="order-title">
-				<span>item</span>
-				<span>num</span>
-				<span>total price</span>
-			</div>
-			<div id="order-info">
-				<div id="item">
-					<img src="" alt="item pic"><span>item Name</span>
-				</div>
-				<div id="num">
-					<span>item nu</span>
-				</div>
-				<div id="total-price">
+			<div id="order-detail">
+				<div id="order-title">
+					<span>item</span>
+					<span>num</span>
 					<span>total price</span>
+				</div>
+				<div id="order-info">
+					<div id="item">
+						<img src="" alt="item pic"><span>item Name</span>
+					</div>
+					<div id="num">
+						<span>item nu</span>
+					</div>
+					<div id="total-price">
+						<span>total price</span>
+					</div>
 				</div>
 			</div>
 			<div id="address">
@@ -59,15 +61,48 @@
 					<span>Your City:</span>
 					<input type="text" name="city" holdplace="" maxlength="30">
 					<span>Your State:</span>
-					<input type="text" name="state" holdplace="" maxlength="">
+					<input type="text" name="state" holdplace="" maxlength="20">
 					<span>Your Country:</span>
 					<input type="text" name="country" holdplace="" maxlength="20">
 					<span>Your Zip:</span>
-					<input type="number" name="zip" holdplace="" maxlength="10">
+					<input type="text" name="zip" holdplace="" maxlength="10">
 					<input type="submit" value="Payment">
 				</form>
 			</div>
 		</div>
+		<footer>
+			<div id="footer">
+				<div id="footer">
+					<div class="footer1">
+						<ul class="website">
+							<h4 class="foot-title">Get To Know Us</h4>
+							<li><a href="#">Careers</a></li>
+							<li><a href="#">Investor Relations</a></li>
+							<li><a href="#">Our Planet</a></li>
+							<li><a href="#">Community</a></li>
+						</ul>
+					</div>
+					<div class="footer2">
+						<ul class="website">
+							<h4 class="foot-title">Make Money With Us</h4>
+							<li><a href="#">Sell on TianGou</a></li>
+							<li><a href="#">Advertise Your Products</a></li>
+							<li><a href="#">Become an Affiliate</a></li>
+							<li><a href="#">Independently Publish With Us</a></li>
+						</ul>
+					</div>
+					<div class="footer3">
+						<ul class="website">
+							<h4 class="foot-title">Let Us Help You</h4>
+							<li><a href="#">Your Account</a></li>
+							<li><a href="#">Payment & Refund</a></li>
+							<li><a href="#">Send & Delivery</a></li>
+							<li><a href="#">Help ...</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</footer>
 	</div>	
 </body>
 </html>
